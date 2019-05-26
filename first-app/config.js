3f979a83e6f6f5767a8ac654b8260183baaa5d5e
cfycyf <cfycyf@yeah.net>
2019-05-26 12:42:29 +0800

    配置商城API后端接口，主要使用API工厂作为服务端
    Signed-off-by: cfycyf <cfycyf@yeah.net>

---
+++ first-app/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  version: "1.0.0.1",
+  note: '商城',
+  apiUrl: 'https://api.it120.cc',    //这里配置小程序配置的服务端域名，用于获取数据
+  subDomain: "f345afe19a1031b53ae09fe689de271a", // 如果你的域名是： https://api.it120.cc/abcd 那么这里只要填写 abcd
+  appid: "wx6da41902a4df7094", // 您的小程序的appid，购物单功能需要使用
+}
---
+++ first-app/wxapi/config.js
@@ -0,0 +1,7 @@
+const CONFIG = require('../config.js')
+module.exports = {
+  version: '1.0.1',
+  note: '同时支持 html 和 小程序',
+  apiUrl: CONFIG.apiUrl,
+  subDomain: CONFIG.subDomain
+}
